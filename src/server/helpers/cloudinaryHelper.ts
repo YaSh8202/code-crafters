873753d22e7ecd82497189ab3d335e5e776d00f1
Yash <bajajy82@gmail.com>
2023-04-12 19:14:45 +0530

    fixed puppeteer

--- src/server/helpers/cloudinaryHelper.ts
+++ src/server/helpers/cloudinaryHelper.ts
@@ -1,7 +1,9 @@
 import { type UploadApiResponse, v2 as cloudinary } from "cloudinary";
 import { Readable } from "stream";
 import { env } from "~/env.mjs";
-import chromium from "chrome-aws-lambda";
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+
 
 cloudinary.config({
   cloud_name: env.CLOUDINARY_NAME,
@@ -28,52 +30,58 @@ const bufferUpload = async (buffer: Buffer) => {
   });
 };
 
-async function getBrowserInstance() {
-  const executablePath = await chromium.executablePath;
+// async function getBrowserInstance() {
+//   const executablePath = await chromium.executablePath;
 
-  // if (!executablePath) {
-  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
-  //   const puppeteer = require('puppeteer');
+//   // if (!executablePath) {
+//   //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+//   //   const puppeteer = require('puppeteer');
 
-  //   // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  //   return puppeteer.launch({
-  //     args: chromium.args,
-  //     headless: true,
-  //     defaultViewport: {
-  //       width: 1280,
-  //       height: 720,
-  //     },
-  //     ignoreHTTPSErrors: true,
-  //   });
-  // }
+//   //   // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+//   //   return puppeteer.launch({
+//   //     args: chromium.args,
+//   //     headless: true,
+//   //     defaultViewport: {
+//   //       width: 1280,
+//   //       height: 720,
+//   //     },
+//   //     ignoreHTTPSErrors: true,
+//   //   });
+//   // }
 
-  return chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: {
-      width: 1280,
-      height: 720,
-    },
-    executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-}
+//   return chromium.puppeteer.launch({
+//     args: chromium.args,
+//     defaultViewport: {
+//       width: 1280,
+//       height: 720,
+//     },
+//     executablePath,
+//     headless: chromium.headless,
+//     ignoreHTTPSErrors: true,
+//   });
+// }
 
 export async function getScreenshot(url: string): Promise<string> {
-  let browser = null;
-
-  try {
-    browser = await getBrowserInstance();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const imageBuffer = await page.screenshot();
-    const { secure_url } = (await bufferUpload(
-      imageBuffer as Buffer
-    )) as UploadApiResponse;
-    console.log("secure_url", secure_url);
-    return secure_url;
-  } catch (e) {
-    console.error(e);
-    return "";
-  }
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === 'win32'
+            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+            : process.platform === 'linux'
+            ? '/usr/bin/google-chrome'
+            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      };
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 2000, height: 1000 });
+  await page.goto(url, { waitUntil: 'networkidle0' });
+  const buffer =  await page.screenshot() as Buffer;
+  const {secure_url } = await bufferUpload(buffer) as UploadApiResponse;
+  return secure_url;
 }

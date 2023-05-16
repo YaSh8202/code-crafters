a23d22ac333812b2bc5f7265b540e883af77829e
Yash <bajajy82@gmail.com>
2023-05-16 15:18:43 +0530

    updated cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://code-crafters.vercel.app",
   },
 });
